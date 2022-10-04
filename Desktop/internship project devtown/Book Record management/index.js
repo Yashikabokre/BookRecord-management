9c2d158225c181803b7a491416bab5c29d650747
Yashi <yashibokre2001@gmail.com>
2022-10-04 12:30:33 +0530

    completed project setup

--- Desktop/internship project devtown/Book Record management/index.js
+++ Desktop/internship project devtown/Book Record management/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {users} = require("./data/users.json");   // json data import
 
 const app = express();
 
@@ -12,6 +13,49 @@ app.get("/", (req, res) => {
     });
 });
 
+/**
+ * Route: /users
+ * Method: GET
+ * Description : Get all user
+ * Access: Public 
+ * Parameters: None 
+ */
+
+app.get("/users", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: users,
+    });
+});
+
+/**
+ * Route: /users/:id
+ * Method: GET✅
+ * Description : Get single user by id  
+ * Access: Public 
+ * Parameters: id
+ */
+
+app.get("/users/:id", (req,res) =>{ 
+    const {id} = req.params;
+    const users = users.find((each) => each.id === id);
+    if(!users) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+    }
+
+    return res.status(200).json({
+        success: true,
+        data: users
+    });
+
+});
+
+
+
+
 app.get("*", (req, res) => {
         res.status(404).json({
             message: "This route does not exist",
@@ -21,3 +65,4 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
 });
+
