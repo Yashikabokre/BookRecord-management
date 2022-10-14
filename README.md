a4af1377a9ec832f95a68f628ce9c9e23617adaa
Yashika bokre <83172795+Yashikabokre@users.noreply.github.com>
2022-10-15 02:05:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # BookRecord-management
-this is a book record management API Backend for management of record and books
+
+This is a book record management API Backend for management of record and books.
+
+# API Documentation link
+
+https://documenter.getpostman.com/view/23232404/2s83zcTSNC
+
+# Routes and End point
+
+## /users
+
+POST: Create a new user
+GET: Get all list of users
+
+## /users/{id}
+
+GET: Get a user by id
+PUT: Update a user by id
+DELETE: Delete a user by id (check if he/she still has an issued book.) (is there any fine to be paid)
+
+## /users/subscription-details/{id}
+
+GET: users subsription detail
+
+1. Date of subscription
+2. Valid till
+3. Fine if any
+
+## /books
+
+GET: Get all the book
+POST: Create/add new book
+
+## /book/{id}
+
+GET: Get a book by id
+PUT: update a book by id
+
+## /book/issued/by-user(books)
+
+GET: Get all isued book
+
+## /book/issued/withFine
+
+//TODO TASK//
+GET: Get all issued book with fine
+
+# Subscription Type
+
+Basic (3 months)
+Standard (6 months)
+Premium (12 months)
+
+NOTE: dates will be in format mm/dd/yyyy
+
+If the subscription date is 01/08/22 and Subscription type is Standard the valid till date will be 01/02/23
+
+If he has an issued book and the issued book is to be returned at 01/01/23 If he missed the date of return, then he gts a fine of Rs. 100./
+
+If he has an issued book and the issued book is to be returned at 01/01/23 If he missed the date of return, and his subscription also expires, then he will get a fine of Rs 200./
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const {users} = require("./data/users.json");   // json data import
+
+//importing routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
 
 const app = express();
 
@@ -12,6 +16,9 @@ app.get("/", (req, res) => {
         message: "Server is up and running",
     });
 });
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
 
 /**
  * Route: /users
@@ -53,7 +60,99 @@ app.get("/users/:id", (req,res) =>{
 
 });
 
+/**
+ * Route: /users/:id
+ * Method: POST
+ * Description : Create new user
+ * Access: Public 
+ * Parameters: none
+ */
 
+app.post("/users", (req,res) => {
+    const {id, name , surname, email, subscriptionType, subscriptionDate} = 
+        req.body;
+
+    const user = users.find((each) => each.id === id)
+
+    if(user) {
+        return res.status(404).json({
+            success: false,
+            message: "User exists with this id",
+        });
+    }
+
+    users.push({
+        id,
+        name,
+        surname,
+        email,
+        subscriptionType,
+        subscriptionDate,
+    });
+
+    return res.status(201).json({
+        success: true,
+        data: users,
+    });
+});
+
+/**
+ * Route: /users/:id
+ * Method: Put
+ * Description : Updating user data
+ * Access: Public 
+ * Parameters: id
+ */
+
+app.put("/users/:id", (req,res) => {
+    const {id} = req.params;
+    const {data} = req.body;
+
+    const user = users.find((each) => each.id === id);
+
+    if(!user) 
+        return res.status(404).json({success: false, message: "User not found"});
+
+    const updateUser = users.map((each) => {
+        if (each.id === id){
+            return{
+                ...each,
+                ...data,
+            };
+        }
+        return each;
+    });
+
+    return res.status(200).json({
+        success: true,
+        data: updateUser,
+    });
+});
+
+/**
+ * Route: /users/:id
+ * Method: Delete
+ * Description : Delete a user by id
+ * Access: Public 
+ * Parameters: id
+ */
+
+app.delete('/users/:id', (req,res) => {
+    const {id} = req.params;
+    const user = users.find((each) => each.id === id);
+
+    if(!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User to be deleted was not found",
+        });
+    }
+
+    const index = users.indexOf(user);
+    users.splice(index, 1);
+
+    return res.status(202).json({success: true, data: users });
+});
 
 app.get("*", (req, res) => {
         res.status(404).json({
@@ -64,4 +163,3 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
 });
-
